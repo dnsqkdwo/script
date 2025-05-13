2b842a16009477c1e0cb2325d60b75ca057b0d65
dnsqkdwo <q54810679@gmail.com>
2025-05-13 22:24:58 +0900

    Add files via upload

---
+++ declaring_variable.js
@@ -0,0 +1,26 @@
+// Variable 선언하기 
+// var - 더 이상 쓰지 않음 , let , const
+
+var name = "방재운";
+console.log(name);
+
+var age = 18;
+console.log(age);
+
+let ive = "아이브";
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것 
+ * 2) 할당
+ */
+
+let girlfriend;
+console.log(girlfriend); 
---
+++ naming_convention.js
@@ -0,0 +1,29 @@
+/**
+ *Naming Conventions
+
+ 1) 영어를 사용하면 문자와 숫자를 모두 사용 가능
+ 2) 특수기호는 _ 와 $만 사용 가능
+ 3) 숫자로 이름을 시작할 수 없다
+ 4) 키워드는 변수명으로 사용 할 수 없다
+ */
+
+let name = "방재운";
+var $sport = "축구";
+const _cook = "요리";
+
+console.log(name);
+console.log($sport);
+console.log(_cook);
+
+// 가능
+
+// let 1cook = "요리" 불가능 . 3
+// var const = "아무거나" 불가능 . 4
+
+/**
+ * Naming Conventions2
+ * 
+ * 1) camelCase = 대부분 언어에서 사용 (띄어쓰기 후 첫번째 글자 대문자로 사용) 
+ * 2) snake_case = 파이썬같은 언어에서 사용
+ * 3) PascalCase = C#에서 많이 사용
+ */
